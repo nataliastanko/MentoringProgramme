9405b44744dccb810a28d7f25eade8c1f093260b
Natalia Stanko <anithaly@gmail.com>
2018-03-25 08:06:41 +0100

    organization editable for admin

---
+++ app/DoctrineMigrations/Version20180325063513.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180325063513 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE organizations ADD logoFileName VARCHAR(255) DEFAULT NULL, ADD url VARCHAR(255) DEFAULT NULL, ADD contact_email VARCHAR(255) NOT NULL, ADD partners_email VARCHAR(255) NOT NULL, ADD description LONGTEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE organizations DROP logoFileName, DROP url, DROP contact_email, DROP partners_email, DROP description');
+    }
+}
