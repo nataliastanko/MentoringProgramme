9f8f0931c1e4590781f7d4578337a91cc91db7ef
Natalia Stanko <anithaly@gmail.com>
2019-11-13 22:01:23 +0000

    enable profiler

--- config/bundles.php
+++ config/bundles.php
@@ -24,4 +24,5 @@ return [
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
     DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
