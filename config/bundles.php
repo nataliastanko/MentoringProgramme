93c507460ded82b4dd80a3c15355befdbcd578c4
Natalia Stanko <anithaly@gmail.com>
2019-11-15 18:25:05 +0000

    configure easyadmin

--- config/bundles.php
+++ config/bundles.php
@@ -26,4 +26,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
